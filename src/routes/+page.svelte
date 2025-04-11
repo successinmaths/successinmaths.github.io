7bee07405c07a37cac52f0a370f2a0b1a7596257
sjz <simon@zinsli.co>
2025-04-11 23:01:35 +1000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
     <img class="m-auto block w-full max-w-lg" src="/logo-large.png" alt="Success In Maths" />
     <div class="m-auto text-center text-lg sm:text-base">
 	Inquiries: <a class="hover:underline" href="mailto:admin@successinmaths.com.au">admin@successinmaths.com.au</a>
+    </div>
+    <div class="m-auto text-center text-lg sm:text-base">
         <a class="hover:grayscale" href="https://www.facebook.com/profile.php?id=61568327004662"><img class="inline w-8 mx-4" src="/facebook.svg" alt="Success In Maths Facebook"></a>
         <a class="hover:grayscale" aria-label="Chat on WhatsApp" href="https://wa.me/61468473016"><img class="inline w-8 mx-4" alt="Chat on WhatsApp" src="/WhatsApp.svg" /></a>
     </div>
