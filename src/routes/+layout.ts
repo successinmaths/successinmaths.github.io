d95d0d4b6cba8ccdeae7141d440137358beec835
Simon Zinsli <simon@zinsli.co>
2025-04-12 14:16:03 +1000

    style updates

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,4 @@
 export const ssr = false;
 export const prerender = true;
+/* Import FontAwesome CSS */
+import '@fortawesome/fontawesome-free/css/all.css';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <div class="w-full">
-    <img class="m-auto block w-full max-w-lg" src="/logo-large.png" alt="Success In Maths" />
-    <div class="m-auto text-center text-lg sm:text-base">
-	Inquiries: <a class="hover:underline" href="mailto:admin@successinmaths.com.au">admin@successinmaths.com.au</a>
-    </div>
-    <div class="m-auto text-center text-lg sm:text-base">
-        <a class="hover:grayscale" href="https://www.facebook.com/profile.php?id=61568327004662"><img class="inline w-8 mx-4" src="/facebook.svg" alt="Success In Maths Facebook"></a>
-        <a class="hover:grayscale" aria-label="Chat on WhatsApp" href="https://wa.me/61468473016"><img class="inline w-8 mx-4" alt="Chat on WhatsApp" src="/WhatsApp.svg" /></a>
-    </div>
+    <img class="m-auto block w-full max-w-md" src="/logo-large.png" alt="Success In Maths" />
+    <div class="m-auto text-center text-xl sm:text-lg">
+	<span class="mx-1">Inquiries:</span>
+    <a class="mx-1 " href="mailto:admin@successinmaths.com.au" aria-label="Email to admin@successinmaths.com.au">
+        <i class="fa-regular fa-envelope text-[#BB001B] hover:text-black transition duration-300"></i>
+    </a>
+    
+     <a class="mx-1" href="https://www.facebook.com/profile.php?id=61568327004662" aria-label="Visit our Facebook page">
+        <i class="fa-brands fa-facebook text-[#3b5998] hover:text-black transition duration-300"></i>
+    </a>
+
+    <a class="mx-1" href="https://wa.me/61468473016" aria-label="Chat on WhatsApp">
+        <i class="fa-brands fa-whatsapp text-[#25D366] hover:text-black transition duration-300"></i>
+    </a>
+
+</div>
+
+
 
 </div>
